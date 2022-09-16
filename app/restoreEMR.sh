99f9f12ce0c8ff23ccf4c911cccb4c3e817a49bc
Kevin Haggerty <khaggerty@signiant.com>
2022-09-16 16:44:25 -0400

    Revert to emr 5.22 and m5a.2xlarge instance type

--- app/restoreEMR.sh
+++ app/restoreEMR.sh
@@ -107,7 +107,7 @@ if [ $NEXTPHASE == 1 ]; then
                 logMsg "Creating new EMR Cluster NAME:${CLUSTER_NAME} Attempt ${CURR_ATTEMPT} of ${RETRIES}"
 
                 CLUSTERID=$(aws emr create-cluster --name "${CLUSTER_NAME}"                                        \
-                            --release-label "emr-5.36.0"                                                           \
+                            --release-label "emr-5.22.0"                                                           \
                             --service-role "EMR_DefaultRole"                                                       \
                             --security-configuration "dynamodb-backups"                                            \
                             --tags Name=${CLUSTER_NAME} signiant:product=devops signiant:email=devops@signiant.com \
